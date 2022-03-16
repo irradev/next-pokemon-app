5afede26a9815301f60e243a8c56ef04d41609e1
lazypolarbear <ezvaseg@gmail.com>
2022-03-16 03:04:18 -0600

    Navbar actualizado 1.0

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
       <NextLink href="/" passHref>
         <Link>
           <Text color="white" h2>
-            ¡P
+            P
           </Text>
           <Text color="white" h3>
             okémon!
